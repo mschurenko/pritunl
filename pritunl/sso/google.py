f54df0be322a6d1496b89c85c7424e1660630e0d
Matt Schurenko <matt@trinimbus.com>
2016-02-07 23:06:19 -0800

    testing google.py

--- pritunl/sso/google.py
+++ pritunl/sso/google.py
@@ -1,5 +1,9 @@
 from pritunl import settings
 
 def verify_google(user_email):
-    user_domain = user_email.split('@')[-1]
-    return user_domain in settings.app.sso_match, None
+    user, user_domain = user_email.split('@')
+    if user_domain in settings.app.sso_match and user == 'foo':
+      r_value = True
+    else:
+      r_value = False
+    return r_value, None
